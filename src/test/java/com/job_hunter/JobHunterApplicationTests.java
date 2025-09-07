da9da4600262d54aac63c71cbb924d74fbe3cf39
nhantran <nhantran@minhhop.net>
2025-09-07 23:37:11 +0700

    first commit

---
+++ src/test/java/com/job_hunter/JobHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_hunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
