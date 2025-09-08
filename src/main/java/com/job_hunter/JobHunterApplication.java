186d57268b02649b3cff60d1bc7efec2272c3995
nhantran <nhantran@minhhop.net>
2025-09-08 17:26:29 +0700

    implement test

--- src/main/java/com/job_hunter/JobHunterApplication.java
+++ src/main/java/com/job_hunter/JobHunterApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobHunterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobHunterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello 2");
+        SpringApplication.run(JobHunterApplication.class, args);
+    }
 
 }
