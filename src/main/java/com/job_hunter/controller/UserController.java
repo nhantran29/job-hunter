554a427cca1ea85ec718f1cc7bb9e92696e60acd
nhantran <nhantran@minhhop.net>
2025-09-11 23:52:43 +0700

    Still try to get connect mysql

--- src/main/java/com/job_hunter/controller/UserController.java
+++ src/main/java/com/job_hunter/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.job_hunter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @RestController
 public class UserController {
+//    @Autowired
+    private DataSource dataSource;
 
-    @GetMapping("/users")
+    @GetMapping("/test-db")
+    public String testDb() throws SQLException {
+        return dataSource.getConnection().getMetaData().getURL();
+    }
+    @GetMapping("/")
     public String users() {
         return "userss";
     }
