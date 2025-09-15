b14163244671c63ca79b4ea960b2d78746e3265d
nhantran <nhantran@minhhop.net>
2025-09-15 09:20:48 +0700

    connected to mysql

--- src/main/java/com/job_hunter/domain/JHEntity.java
+++ src/main/java/com/job_hunter/domain/JHEntity.java
@@ -2,11 +2,21 @@ package com.job_hunter.domain;
 
 import jakarta.persistence.*;
 
-@Entity
+import java.util.Date;
+
+//@Entity
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // hoặc TABLE_PER_CLASS
+@MappedSuperclass
 public class JHEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "last_updated")
+    private Date lastUpdated;
 }
